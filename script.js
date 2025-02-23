0afbec11d1efb73d7af8d3a24992c92271d3ed73
Jason198341 <skypeople41@gmail.com>
2025-02-23 18:00:13 +0530

    "feat: 숫자 색상 변경 기능 추가"

--- script.js
+++ script.js
@@ -19,3 +19,11 @@ function reset() {
     count = 0;
     updateDisplay();
 }
+
+
+// script.js에 추가
+function changeColor() {
+    const colors = ['red', 'blue', 'green', 'purple'];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    counterDisplay.style.color = randomColor;
+}
